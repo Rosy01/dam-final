4fef35f417c5bd06626a519a250e954a642bea9a
Your Name <you@example.com>
2023-07-13 02:15:46 -0500

    avance 4

---
+++ app/src/main/java/com/example/app_calendario/AddCursos.java
@@ -0,0 +1,14 @@
+package com.example.app_calendario;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class AddCursos extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_cursos);
+    }
+}
