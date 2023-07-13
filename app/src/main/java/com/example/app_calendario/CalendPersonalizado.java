8bd20b38f532c7461c48189c2ed5b5c75c6b65cb
Your Name <you@example.com>
2023-07-12 23:44:36 -0500

    avance 3

---
+++ app/src/main/java/com/example/app_calendario/CalendPersonalizado.java
@@ -0,0 +1,14 @@
+package com.example.app_calendario;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class CalendPersonalizado extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_calend_personalizado);
+    }
+}
---
+++ app/src/main/java/com/example/app_calendario/horariosCursos.java
@@ -0,0 +1,14 @@
+package com.example.app_calendario;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class horariosCursos extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_horarios_cursos);
+    }
+}
